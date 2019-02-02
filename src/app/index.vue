ba05a61f163bf243ece5e67cf8d1136e7d2a6e33
Rafe Goldberg <rafegoldberg@gmail.com>
2019-02-02 11:24:25 -0800

    style

--- src/app/index.vue
+++ src/app/index.vue
@@ -139,6 +139,7 @@ blockquote {
 .App {
   padding-left: env(safe-area-inset-left);
   padding-right: env(safe-area-inset-right);
+  padding-bottom: env(safe-area-inset-bottom);
   & {
     display: flex;
     flex-flow: nowrap column;
